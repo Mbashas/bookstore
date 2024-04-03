3353eea2036ac8b66b68874ffd20694ce583fb60
sethmbasha <sethmbasha@gmail.com>
2024-04-03 21:39:20 +0300

    first commit

---
+++ template/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $title; ?></title>
+
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #00a3cc; height: 80px;">
+      <a class="navbar-brand" href="#">
+        <img src="./bootstrap/img/5.png" alt="Logo" style="height:80px;">
+    </a>
+    </a>
+      <div class="container">
+        <div class="navbar-header" style="background-color: #00a3cc;">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">SABLE PAGES</a>
+        </div>
+
+        <!--/.navbar-collapse -->
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <!-- link to publiser_list.php -->
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Publisher</a></li>
+              <!-- link to books.php -->
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Books</a></li>
+              <!-- link to contacts.php -->
+                <!--<li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li>
+                <!-- link to shopping cart -->
+                <!--<li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; My Cart</a></li>-->
+            </ul>
+        </div>
+      </div>
+    </nav>
+    <?php
+      if(isset($title) && $title == "Index") {
+    ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to SABLE Pages - Your Online Bookstore</h1>
+        <p class="lead">Discover a world of knowledge at our bookstore.</p>
+        <p>Immerse yourself in a vast library and find yourself in the pages of countless stories.</p>
+      </div>
+    </div>
+    <?php } ?>
+
+    <div class="container" id="main">
